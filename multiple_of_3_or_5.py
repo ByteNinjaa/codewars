24b5e2db5c7c845d5d02a978bfd8a19400961438
ByteNinjaa <45305495+ByteNinjaa@users.noreply.github.com>
2024-04-23 21:13:12 -0400

    Create multiple_of_3_or_5.py

---
+++ multiple_of_3_or_5.py
@@ -0,0 +1,14 @@
+def solution(number):
+    if number <= 0:
+        return 0
+    
+    sum = 0
+    for i in range(number):
+        if i % 3 == 0 and i % 5 == 0:
+            sum += i
+        elif i % 3 == 0:
+            sum += i
+        elif i % 5 == 0:
+            sum += i
+    
+    return sum
